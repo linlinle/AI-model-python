4e393c8935c69736319588016a6657afcb56d569
linlinle <linlinle@outlook.com>
2019-04-09 16:23:15 +0800

    Remove redundancy

--- TF_Tutorial/Embeddings/Embeddings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import tensorflow as tf
-
-vocabulary_size = 5
-embedding_size = 100
-word_ids = [1,2,3,4,5]
-word_embeddings = tf.get_variable('word_embeddings',
-    [vocabulary_size, embedding_size])
-embedded_word_ids = tf.nn.embedding_lookup(word_embeddings, word_ids)
--- TF_Tutorial/Tensors/types.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-'''To inspect a tf.Tensor's data type use the Tensor.dtype property.'''
-import tensorflow as tf
-
-# Cast a constant integer tensor into floating point.
-float_tensor = tf.cast(tf.constant([1, 2, 3]), dtype=tf.float32)
